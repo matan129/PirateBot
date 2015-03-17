d97f4c3af381846be69210577c7cbac644ff8bec
Matan R <matan129@gmail.com>
2015-03-17 16:56:00 +0200

    Added classes for enemy tracking and a class for useful extension methods

---
+++ starter_kit/better-bots/WeekOneBot/WeekOneBot/EnemyFleet.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Pirates;
+
+namespace WeekOneBot
+{
+    class EnemyFleet
+    {
+        private List<EnemyPirate> enemies;
+    }
+}
---
+++ starter_kit/better-bots/WeekOneBot/WeekOneBot/PiratesExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Pirates;
+
+namespace WeekOneBot
+{
+    static class PiratesExtensions
+    {
+        public static Island GetIsland(this IPirateGame Game, Location Location)
+        {
+            foreach (Island isle in Game.Islands())
+            {
+                if (isle.Loc == Location)
+                {
+                    return isle;
+                }
+            }
+            return null;
+        }
+    }
+}
