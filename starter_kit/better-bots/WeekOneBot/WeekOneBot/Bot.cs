8404327abe235d7cc4a1f6faad6cf7769197ae7f
Matan R <matan129@gmail.com>
2015-03-19 10:12:59 +0200

    Added internet connection checker

--- starter_kit/better-bots/WeekOneBot/WeekOneBot/Bot.cs
+++ starter_kit/better-bots/WeekOneBot/WeekOneBot/Bot.cs
@@ -1,9 +1,12 @@
 ﻿using System;
+using System.Net;
+using System.Net.NetworkInformation;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Pirates;
+
 
 namespace WeekOneBot
 {
@@ -21,11 +24,33 @@ namespace WeekOneBot
            
             if (!inited)
             {
+				IsConnectedToInternet();
                 this.commander.Distribute(Ai.GetBestConfig());
                 this.inited = true;
             }
 
             this.commander.Play();
         }
+		
+		public bool IsConnectedToInternet()
+        {
+		    Game.Debug("Checking connection...");
+            //if connected we can save logs between games and adapt the strategy
+            try
+			{
+				using (var client = new WebClient())
+				using (var stream = client.OpenRead("http://www.google.com"))
+				{
+					Game.Debug("Connected to Google!");
+					return true;
+				}
+			}
+			catch
+			{
+				Game.Debug("Could not connect");
+				return false;
+			}
+		}		
     }
+    
 }
