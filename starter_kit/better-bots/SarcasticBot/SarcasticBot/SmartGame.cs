8604c1eaa00cc7af4fb8efa4c3c11f56a4284af3
tzvika <tzvika.tubis@gmail.com>
2015-03-26 19:38:19 +0200

    pirate moves without game partition

--- starter_kit/better-bots/SarcasticBot/SarcasticBot/SmartGame.cs
+++ starter_kit/better-bots/SarcasticBot/SarcasticBot/SmartGame.cs
@@ -1,5 +1,5 @@
 ﻿using System.Collections.Generic;
-
+using Pirates;
 namespace SarcasticBot
 {
     public static class SmartGame
@@ -7,5 +7,15 @@ namespace SarcasticBot
         public static List<SmartIsland> SmartIslands;
         public static List<FriendPirate> MySmartPirates;
         public static List<EnemyPirate> EnemySmartPirates;
+
+        //pirates
+       public static Pirate GetMyPirate(int index)
+        {
+            return Bot.Game.GetMyPirate(index);
+        }
+       public static Pirate GetEnemyPirate(int index)
+       {
+           return Bot.Game.GetEnemyPirate(index);
+       }
     }
 }
