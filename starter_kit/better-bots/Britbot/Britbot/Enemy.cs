fe407791e4181a16dbfb69b93012ca0c6c130e1b
Matan R <matan129@gmail.com>
2015-03-28 16:16:38 +0300

    Started implementing the enemy classes

--- starter_kit/better-bots/Britbot/Britbot/Enemy.cs
+++ starter_kit/better-bots/Britbot/Britbot/Enemy.cs
@@ -2,25 +2,39 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Pirates;
 
 namespace Britbot
 {
     public static class Enemy
     {
-        public static List<EnemyGroup> Groups
-        {
-            get
-            {
-                throw new System.NotImplementedException();
-            }
-            set
-            {
-            }
-        }
-
+        public static List<EnemyGroup> Groups { get; private set; }
+    
+        /// <summary>
+        /// Split the enemy into its groups
+        /// </summary>
         public static void AnalyzeConfig()
         {
-            throw new System.NotImplementedException();
+            List<EnemyGroup> groups = new List<EnemyGroup>();
+            foreach (Pirate pete in Bot.Game.AllEnemyPirates().Where(p => !p.IsLost))
+            {
+                EnemyGroup newGroup = new EnemyGroup();
+                newGroup.EnemyPirates.Add(pete.Id);
+                
+                //TODO May be more efficient
+                List<EnemyGroup> containsPete = groups.Where(g => g.IsInGroup(pete.Id)).ToList();
+                if (containsPete.Count > 0)
+                {
+                    groups.RemoveAll(g => g.IsInGroup(pete.Id));
+                    foreach (EnemyGroup gr in containsPete)
+                    {
+                        newGroup.EnemyPirates.AddRange(gr.EnemyPirates);
+                    }
+                }
+                groups.Add(newGroup);
+            }
+
+            Groups = groups;
         }
     }
 }
--- starter_kit/better-bots/Britbot/Britbot/EnemyGroup.cs
+++ starter_kit/better-bots/Britbot/Britbot/EnemyGroup.cs
@@ -2,41 +2,58 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Pirates;
 
 namespace Britbot
 {
+    /// <summary>
+    /// A class which represents an enemy group
+    /// </summary>
     public class EnemyGroup : ITarget
     {
-        public List<int> EnemyPirates
-        {
-            get
-            {
-                throw new System.NotImplementedException();
-            }
-            set
-            {
-            }
-        }
+        /// <summary>
+        /// List of pirate indexes in this group
+        /// </summary>
+        public List<int> EnemyPirates { get; private set; }
 
-        public HeadingVector Heading
-        {
-            get
-            {
-                throw new System.NotImplementedException();
-            }
-            set
-            {
-            }
-        }
+        /// <summary>
+        /// The direction this group's heading to 
+        /// </summary>
+        public HeadingVector Heading { get; private set; }
     
+        /// <summary>
+        /// Gets the score for this group
+        /// </summary>
+        /// <param name="origin">The group requesting the evaluation</param>
+        /// <returns>The score for this group</returns>
         public Score GetScore(Group origin)
         {
             throw new NotImplementedException();
         }
 
-        public Pirates.Location GetLocation()
+        /// <summary>
+        /// Returns the average location for this group
+        /// </summary>
+        /// <returns>Returns the average location for this group</returns>
+        public Location GetLocation()
         {
-            throw new NotImplementedException();
+            int totalRow = 0, totalCol = 0;
+            
+        }
+
+        /// <summary>
+        /// Determines if an enemy pirate belongs to this enemy group.
+        /// </summary>
+        /// <param name="enemyPirate">The index of the enemy pirate</param>
+        /// <returns>True if the pirate belongs to the group or false, otherwise</returns>
+        public bool IsInGroup(int enemyPirate)
+        {
+            Pirate ePirate = Bot.Game.GetEnemyPirate(enemyPirate);
+            return
+                this.EnemyPirates.ConvertAll(e => Bot.Game.GetEnemyPirate(e))
+                    .Select(ep => Bot.Game.Distance(ep, ePirate))
+                    .Concat(new int[] {})
+                    .Min() <= 2;
         }
     }
 }
