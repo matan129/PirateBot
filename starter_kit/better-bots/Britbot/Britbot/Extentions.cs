3f4d3b37f7048653170716e3154316a8b3261f20
Matan R <matan129@gmail.com>
2015-03-28 23:54:30 +0300

    Fixed the island encapsulating issue and added two useful extension methods for the IPirateGame interface

--- starter_kit/better-bots/Britbot/Britbot/Extentions.cs
+++ starter_kit/better-bots/Britbot/Britbot/Extentions.cs
@@ -1,6 +1,32 @@
-﻿namespace Britbot
+﻿using System.Collections.Generic;
+using Pirates;
+
+namespace Britbot
 {
+    /// <summary>
+    /// This class provied various extension methods for the IPirateGame interface
+    /// </summary>
     public static class Extensions
     {
+        /// <summary>
+        /// Gets a SmartIsland by index, as we would do with a normal Island
+        /// </summary>
+        /// <param name="game"></param>
+        /// <param name="Id">The SmartIsland's ID, which is equal to the ID of the normal island it encapsulates</param>
+        /// <returns>The SmartIsland with the relevant ID</returns>
+        public static SmartIsland GetSmartIsland(this IPirateGame game, int Id)
+        {
+            return SmartIsland.IslandList.Find(isle => isle.Id == Id);
+        }
+
+        /// <summary>
+        /// Gets the list of SmartIsland like we would normally do with normal Islands
+        /// </summary>
+        /// <param name="game"></param>
+        /// <returns>A list containing all the SmartIslands in game</returns>
+        public static List<SmartIsland> SmartIslands(this IPirateGame game)
+        {
+            return SmartIsland.IslandList;
+        }
     }
 }
--- starter_kit/better-bots/Britbot/Britbot/SmartIsland.cs
+++ starter_kit/better-bots/Britbot/Britbot/SmartIsland.cs
@@ -5,23 +5,139 @@ using Pirates;
 
 namespace Britbot
 {
-    public class SmartIsland : Island, ITarget
+    public class SmartIsland :  ITarget
     {
-        public SmartIsland(int Id, Location Loc, int Owner, int TeamCapturing, int TurnsBeingCaptured, int CaptureTurns,
-            int Value)
-            : base(Id, Loc, Owner, TeamCapturing, TurnsBeingCaptured, CaptureTurns, Value)
+        #region constructors
+        /// <summary>
+        /// A static constructor which initializes the static island list on the first reference to a SmartIsland
+        /// </summary>
+        static SmartIsland()
         {
+            IslandList = new List<SmartIsland>();
+            foreach (Island island in Bot.Game.Islands())
+            {
+                IslandList.Add(new SmartIsland(island.Id));
+            }
         }
 
-        public int ID
+        /// <summary>
+        /// Creats a new SmartIsland
+        /// </summary>
+        /// <param name="encapsulate">The regular island index to encapsulate</param>
+        public SmartIsland(int encapsulate)
         {
-            get { return Id; }
+            this.Id = encapsulate;
         }
+        #endregion
 
-        public int CaptureTurns { get; private set; }
-        public int Value { get; private set; }
+        #region members
+        /// <summary>
+        /// A static island list of all the islands in game
+        /// </summary>
         public static List<SmartIsland> IslandList { get; private set; }
-        public Location IslandLocation { get; private set; }
+
+        /// <summary>
+        /// The unique ID of the island
+        /// </summary>
+        public readonly int Id;
+
+        /// <summary>
+        /// Turns to capture the island
+        /// </summary>
+        public int CaptureTurns
+        {
+            get { return Bot.Game.GetIsland(this.Id).CaptureTurns; }
+        }
+
+        /// <summary>
+        /// Value of the island, or how may mnrnal island it is worth
+        /// </summary>
+        public int Value
+        {
+            get { return Bot.Game.GetIsland(this.Id).Value; }
+        }
+
+        /// <summary>
+        /// THe location of the island
+        /// </summary>
+        public Location Loc
+        {
+            get { return Bot.Game.GetIsland(this.Id).Loc; }
+        }
+
+        /// <summary>
+        /// The team currently capturing the island
+        /// </summary>
+        public int TeamCapturing
+        {
+            get { return Bot.Game.GetIsland(this.Id).TeamCapturing; }
+        }
+
+        /// <summary>
+        /// How many turns this island is being captured
+        /// </summary>
+        public int TurnsBeingCaptured
+        {
+            get { return Bot.Game.GetIsland(this.Id).TurnsBeingCaptured; }
+        }
+
+        /// <summary>
+        /// Who owns the island?
+        /// </summary>
+        public int Owner
+        {
+            get { return Bot.Game.GetIsland(this.Id).Owner; }
+        }
+        #endregion
+
+        #region operator overloading and overriding methods
+        /// <summary>
+        /// Checks if 2 smart Islands are equal
+        /// </summary>
+        /// <param name="a">Island 1</param>
+        /// <param name="b">Island 2</param>
+        /// <returns>True if the islands are the same or false otherwise</returns>
+        public static bool operator ==(SmartIsland a, SmartIsland b)
+        {
+            return a.Id == b.Id;
+        }
+
+        /// <summary>
+        /// Checks if 2 smart islands are different
+        /// </summary>
+        /// <param name="a">Island 1</param>
+        /// <param name="b">Island 2</param>
+        /// <returns>True or false</returns>
+        public static bool operator !=(SmartIsland a, SmartIsland b)
+        {
+            return a.Id != b.Id;
+        }
+
+        protected bool Equals(SmartIsland other)
+        {
+            return CaptureTurns == other.CaptureTurns && Value == other.Value && Equals(Loc, other.Loc);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((SmartIsland)obj);
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                int hashCode = base.GetHashCode();
+                hashCode = (hashCode * 397) ^ CaptureTurns;
+                hashCode = (hashCode * 397) ^ Value;
+                hashCode = (hashCode * 397) ^ (Loc != null ? Loc.GetHashCode() : 0);
+                return hashCode;
+            }
+        }
+        #endregion
 
         /// <summary>
         /// Calculates the score of this island relative to a certain group by calculating how many potential points it will generate.
@@ -79,28 +195,6 @@ namespace Britbot
         public Location GetLocation()
         {
             return Loc;
-        }
-
-        /// <summary>
-        /// Checks if 2 smart Islands are equal
-        /// </summary>
-        /// <param name="a">Island 1</param>
-        /// <param name="b">Island 2</param>
-        /// <returns>True if the islands are the same or false otherwise</returns>
-        public static bool operator ==(SmartIsland a, SmartIsland b)
-        {
-            return a.ID == b.ID;
-        }
-
-        /// <summary>
-        /// Checks if 2 smart islands are different
-        /// </summary>
-        /// <param name="a">Island 1</param>
-        /// <param name="b">Island 2</param>
-        /// <returns>True or false</returns>
-        public static bool operator !=(SmartIsland a, SmartIsland b)
-        {
-            return a.ID != b.ID;
         }
 
         /// <summary>
