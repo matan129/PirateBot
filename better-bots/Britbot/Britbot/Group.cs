3a7e784a2da91a4acc87bb072208a82e26595c5b
tomerli97 <tomer.lib@gmail.com>
2015-04-02 18:37:00 +0300

    Added "add pirate" method to the group class

--- better-bots/Britbot/Britbot/Group.cs
+++ better-bots/Britbot/Britbot/Group.cs
@@ -474,6 +474,18 @@ namespace Britbot
 
             Bot.Game.Debug("Priorities Count: " + this.Priorities.Count);
         }
+        /// <summary>
+        /// Adds a pirate to this group, removing it from any other group it was in.
+        /// </summary>
+        /// <param name="index">The pirate's index</param>
+        public void AddPirate(int index)
+        {
+            foreach (Group g in Commander.Groups)
+            {
+                g.Pirates.Remove(index);
+            }
+            this.Pirates.Add(index);
+        }
 
         #region Members
 
@@ -530,4 +542,5 @@ namespace Britbot
 
         #endregion
     }
+
 }
