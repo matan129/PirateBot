5d092d324cee9c392e739e0cc5b21b0658b25d82
Matan Komisarchick <matan1595@gmail.com>
2015-04-08 17:01:21 +0300

    more advancements in the A* stuff

--- better-bots/Britbot/Britbot/Navigator.cs
+++ better-bots/Britbot/Britbot/Navigator.cs
@@ -238,6 +238,11 @@ namespace Britbot
             /// </summary>
             public double H;
 
+            /// <summary>
+            /// this weight represents an impassable cell
+            /// </summary>
+            public static double infinity = 1025583;
+
             //-------------------operators----------------------
 
             /// <summary>
@@ -257,12 +262,141 @@ namespace Britbot
             }
         }
 
-        private Node[,] InitialNodes(int groupStrength)
+        private Node[,] InitialNodes(int groupStrength, Location target)
         {
+            //reading board size
+            int cols = Bot.Game.GetCols();
+            int rows = Bot.Game.GetRows();
+
+            //get the radius of the group
+            int groupRadius = Group.GetRingCount(groupStrength);
+
             //create array of nodes corresponding to the actual locations in the map
-            Node[,] Map = new Node[Bot.Game.GetCols(), Bot.Game.GetRows()];
+            //--------IMPORTANT: the access to the map is like with location- first Y than X !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+            Node[,] Map = new Node[rows, cols];
+
+            //go over the entire map, set locations and their wight
+            for (int x = 0; x < cols; x++)
+            {
+                for (int y = 0; y < rows; y++)
+                {
+                    //set the location inside the node
+                    Map[y, x].Loc = new Location(y, x);
+
+                    //set the huristic parameter to be the regular distance
+                    Map[y, x].H = Bot.Game.Distance(Map[y, x].Loc, target);
+
+                    //set the calculated G parameter to 0
+                    Map[y, x].G = 0;
+
+                    //check if this is passable
+                    if (!Bot.Game.IsPassableEnough(Map[y, x].Loc, groupRadius))
+                    {
+                        //set the weight of the node to "infinity"
+                        Map[y, x].Weight = Node.infinity;
+
+                        //if so then we are finished here, move to next Node
+                        continue;
+                    }
+
+                    //now set the wight based on enemyGroups
+                    double EnemyFactor = 1;
+
+
+                }
+            }
+
 
             return Map;
+        }
+
+        private static double CalcEnemyFactor(Location loc, int GroupStrength)
+        {
+            //constant defining The addvantage factor we have because of our great structure
+            const int addvantageFactor = 1;
+
+            //constants representing the danger distibution across the map (good for enemies we can kill, bad otherwise)
+            const double badDangerSpreadCoeff = 0.5;
+            const double goodDangerSpreadCoeff = 0.1;
+
+            //read attack radious
+            double attackRadius = Bot.Game.GetAttackRadius();
+
+            //constant representing enemyships negative radius on the map
+            double dangerZone = 9 * attackRadius;
+
+            //We would like to avoid enemy groups begger then our selfs and move towords smaller one
+            //But there might be some smaller ones near one another and we won't know that till we 
+            //go over all off them so we keep them in a seperate tab of smaller group and if everything
+            //goes well we subrtact them
+
+            //initialize count
+            double eBadFactor = 1;
+            double eGoodFactor = 1;
+            double eGoodTurnsToBadFactor = 0;
+
+            //enemy count
+            int enemyCount = 0;
+
+            //go over all the enemy groups 
+            foreach (EnemyGroup eGroup in Enemy.Groups)
+            {
+                //get distance of eGroup to the tested location
+                double distanceSquared = eGroup.MinimalSquaredDistanceTo(loc);
+                
+                //read enemy strength
+                int enemyStrength = eGroup.EnemyPirates.Count;
+
+                //check if this group is a threat
+                if(distanceSquared <= dangerZone)
+                {
+                    enemyCount += enemyStrength;
+                }
+                else //otherwise we can skip it
+                {
+                    continue;
+                }
+
+                //we remove one Attack Radious as precaution
+                distanceSquared = Math.Max(0 , distanceSquared - Bot.Game.GetAttackRadius());
+
+                //if they are stronger than us
+                if (enemyStrength - addvantageFactor > GroupStrength)
+                {
+                    //add to bad factor
+
+                    //first we normalize the distance
+                    distanceSquared = distanceSquared / (Node.infinity * (dangerZone - Bot.Game.GetAttackRadius()));
+
+                    //we add in an "inverse to the distant" way and proportional to the enemy group strength
+                    //we also add coefficient so at the edge of the dangerzone it will be badDangerSpreadCoeff * infinity
+                    eBadFactor += badDangerSpreadCoeff * enemyStrength / distanceSquared;
+                }
+                else
+                {
+                    //we add to both the good and the good gone bad just in case
+
+                    //first we normalize the distance
+                    distanceSquared = distanceSquared / (Node.infinity * (dangerZone - Bot.Game.GetAttackRadius()));
+
+                    //we add in an "inverse to the distant" way and proportional to the enemy group strength
+                    //we also add coefficient so at the edge of the dangerzone it will be badDangerSpreadCoeff * infinity
+                    eGoodFactor += goodDangerSpreadCoeff * enemyStrength / distanceSquared;
+                    eGoodTurnsToBadFactor += badDangerSpreadCoeff * enemyStrength / distanceSquared;
+                }
+
+            }
+
+            //check if we are good or naughty 
+            if (enemyCount - addvantageFactor > GroupStrength)
+            {
+                //good
+                return eGoodFactor;
+            }
+            else
+            {
+                return eBadFactor + eGoodTurnsToBadFactor;
+            }
         }
     }
 }
