4d1bc9a03f6058f7f6a14e16c1f9ccc8fb6f271b
Matan R <matan129@gmail.com>
2015-04-20 21:48:52 +0300

    travis?

---
+++ doxybuild.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+REPO_PATH=git@github.com:matan129/PirateBot.git
+CONFIG_PATH=better-bots/Britbot/Britbot/
+HTML_PATH=better-bots/Britbot/Britbot/html
+COMMIT_USER="Doxygen builder at Travis-Ci"
+COMMIT_EMAIL="doxy@gen.com"
+CHANGESET=$(git rev-parse --verify HEAD)
+
+rm -rf ${HTML_PATH}
+mkdir -p ${HTML_PATH}
+
+
+git clone -b gh-pages "${REPO_PATH}" --single-branch ${HTML_PATH}
+cd ${HTML_PATH}
+git rm -rf .
+cd $(CONFIG_PATH)
+
+doxygen BotDoc
+
+cd ${HTML_PATH}
+git add .
+git config user.name "${COMMIT_USER}"
+git config user.email "${COMMIT_EMAIL}"
+git commit -m "Automated documentation build for changeset ${CHANGESET}."
+git push origin gh-pages
+cd -
